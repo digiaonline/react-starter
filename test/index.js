6287ab010c31d0c722c0d1d790a2351a9abc5238
Christoffer Niska <christofferniska@gmail.com>
2016-03-18 13:35:27 +0200

    Improve test suite

--- test/index.js
+++ test/index.js
@@ -1,4 +1,15 @@
 import chai from 'chai';
 import chaiImmutable from 'chai-immutable';
+import chaiEnzyme from 'chai-enzyme';
+import chaiJsx from 'chai-jsx';
+import sinonChai from 'sinon-chai';
+import { jsdom } from 'jsdom';
 
 chai.use(chaiImmutable);
+chai.use(chaiEnzyme());
+chai.use(chaiJsx);
+chai.use(sinonChai);
+
+global.document = jsdom('<!doctype html><html><body></body></html>');
+global.window = document.defaultView;
+global.navigator = global.window.navigator;
