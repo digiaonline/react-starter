9d4662794c5916c1ea83723e214e573944415fe2
Christoffer Niska <christofferniska@gmail.com>
2016-05-05 23:05:34 +0300

    Fix casing issue in logout

--- src/components/logout/logout2.js
+++ src/components/logout/logout2.js
@@ -3,7 +3,7 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
-import { logout } from '../../state/auth/actions';
+import { logout } from '../../state/actions/auth';
 
 export class Logout extends Component {
   componentWillMount() {
