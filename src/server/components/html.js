f2bcd3104ff8780f8ae61598a502087935152c24
Christoffer Niska <christofferniska@gmail.com>
2016-05-11 00:47:01 +0300

    Fix flash of unstyled content in server-side rendering

--- src/server/components/html.js
+++ src/server/components/html.js
@@ -17,12 +17,13 @@ export const Html = ({ assets, component, initialState }) => {
       <meta name="viewport" content="width=device-width, initial-scale=1"/>
       {Object.keys(assets.styles).map((style, i) =>
         <link href={assets.styles[style]} key={i} media="screen, projection" rel="stylesheet" type="text/css"/>)}
+      { !Object.keys(assets.styles).length ? <style dangerouslySetInnerHTML={{ __html: '#root{display:none}' }}/> : null }
     </head>
     <body>
-    <div id="root" dangerouslySetInnerHTML={{ __html: content }}></div>
-    {<script dangerouslySetInnerHTML={{ __html: `window.__INITIAL_STATE__=${JSON.stringify(initialState)};` }}
-             charSet="UTF-8"></script>}
+    <div id="root" dangerouslySetInnerHTML={{ __html: content }}/>
+    {<script dangerouslySetInnerHTML={{ __html: `window.__INITIAL_STATE__=${JSON.stringify(initialState)};` }} charSet="UTF-8"/>}
     {Object.keys(assets.javascript).map((script, i) => <script src={assets.javascript[script]} key={i}></script>)}
+    { !Object.keys(assets.styles).length ? <script dangerouslySetInnerHTML={{ __html: 'document.getElementById("root").style.display="block";' }}/> : null }
     </body>
     </html>
   );
